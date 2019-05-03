b5dea9e49f1bc3d0446ee01fa8bf07eba4c4d12c
ragaey <mohamedragaey33@gmail.com>
2019-05-03 22:03:22 +0200

    Updates

---
+++ precache-manifest.f6b80a465c46f8a76aa0c28b4cd687ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-reads-react/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/my-reads-react/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "2da21051cbd5fdaaace6",
+    "url": "/my-reads-react/static/js/runtime~main.4b80520f.js"
+  },
+  {
+    "revision": "6bd6304931bf54ca43e2",
+    "url": "/my-reads-react/static/js/main.fbff0a14.chunk.js"
+  },
+  {
+    "revision": "1a66219303a522c4ef28",
+    "url": "/my-reads-react/static/js/2.96e534a7.chunk.js"
+  },
+  {
+    "revision": "6bd6304931bf54ca43e2",
+    "url": "/my-reads-react/static/css/main.53242b93.chunk.css"
+  },
+  {
+    "revision": "1a66219303a522c4ef28",
+    "url": "/my-reads-react/static/css/2.ca458d54.chunk.css"
+  },
+  {
+    "revision": "f92d43afaaa73a0b1971cdca22f2b6a7",
+    "url": "/my-reads-react/index.html"
+  }
+];
